f7b90c69bbc124a2f6352e10c79f9b09d904a256
Andrea Cecchi <andrea.cecchi85@gmail.com>
2026-02-03 13:50:27 +0100

    black

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -12,13 +12,11 @@ import transaction
 pytest_plugins = ["pytest_plone"]
 
 globals().update(
-    fixtures_factory(
-        (
-            (INTEGRATION_TESTING, "integration"),
-            (FUNCTIONAL_TESTING, "functional"),
-            (ACCEPTANCE_TESTING, "acceptance"),
-        )
-    )
+    fixtures_factory((
+        (INTEGRATION_TESTING, "integration"),
+        (FUNCTIONAL_TESTING, "functional"),
+        (ACCEPTANCE_TESTING, "acceptance"),
+    ))
 )
 
 
