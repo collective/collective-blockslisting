3ce500d951ab10704fd472a6874dce06d41a1054
Andrea Cecchi <andrea.cecchi85@gmail.com>
2026-02-03 12:19:05 +0100

    Unignore and add backend/src/collective/searchblocks

---
+++ backend/src/collective/searchblocks/setuphandlers/initial.py
@@ -0,0 +1,23 @@
+from collective.searchblocks import logger
+from pathlib import Path
+from plone import api
+
+try:
+    from plone.exportimport import importers
+
+    PLONE_60 = False
+except ImportError:
+    PLONE_60 = True
+from Products.GenericSetup.tool import SetupTool
+
+
+EXAMPLE_CONTENT_FOLDER = Path(__file__).parent / "examplecontent"
+
+
+def create_example_content(portal_setup: SetupTool):
+    """Import content available at the examplecontent folder."""
+    portal = api.portal.get()
+    if not Plone_60:
+        importer = importers.get_importer(portal)
+        for line in importer.import_site(EXAMPLE_CONTENT_FOLDER):
+            logger.info(line)
